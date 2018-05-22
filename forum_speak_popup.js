0401c241d3dfc7d4930f9d9d99d66f73d7ce3417
Thorne Brandt <tbrandt@signal.co>
2018-05-22 18:20:57 -0500

    cleaned up popup

--- forum_speak_popup.js
+++ forum_speak_popup.js
@@ -2,13 +2,6 @@ class ForumPopup {
   constructor(){
     this.speakContentBtn = document.getElementById('speakContent');
     this.infoEl = document.getElementById('info');
-    this.interfaceEl = document.getElementById('interface');
-    this.commentIndicatorEl = document.getElementById('commentIndicator');
-    this.jumpToCommentInput = document.getElementById('jumpToComment');
-    this.jumpToCommentBtn = document.getElementById('jumpToCommentBtn');
-    this.totalCommentsEl = document.getElementById('totalComments');
-    this.currentCommentEl = document.getElementById('currentComment');
-    this.stopBtn = document.getElementById('stopSpeaking');
     chrome.tabs.executeScript({ file: 'forum_speak_content.js', allFrames: false });
     this.addListeners();
   }
@@ -27,87 +20,24 @@ class ForumPopup {
     el.innerHTML = content;
   }
 
-  show(el){
-    el.style.display = 'block';
-  }
-
-  hide(el){
-    el.style.display = 'none';
-  }
-
-  showSpeakBtn(){
-    this.hide(this.stopBtn);
-    this.show(this.speakContentBtn);
-  }
-
-  showStopBtn(){
-    this.hide(this.speakContentBtn);
-    this.show(this.stopBtn);
-  }
-
-  speakContentHandler(){
-    this.showStopBtn();
-    this.sendMessage('speak');
-  }
-
-  stopSpeakingHandler(){
-    this.showSpeakBtn();
-    this.sendMessage('stop');
-  }
-
-  jumpToCommentHandler(){
-    this.currentComment = Number.parseInt(this.jumpToCommentInput.value);
-    if(this.currentComment || this.currentComment == 0){
-      this.sendMessage('jump', this.currentComment);
-      this.replaceContent(this.currentCommentEl, this.currentComment);
-      this.showSpeakBtn();
-    }
-  }
-
   parseFailHandler(){
     this.replaceContent(this.infoEl, 'Sorry, could not parse this page.');
   }
 
-  commentCountHandler(count){
+  closeWindow(){
     window.close();
   }
 
-  currentCommentHandler(currentComment){
-    this.replaceContent(this.currentCommentEl, currentComment);
-  }
-
-  initInterface(speaking){
-    if(speaking){
-      this.hide(this.speakContentBtn);
-      this.show(this.stopBtn);
-    } else {
-      this.hide(this.stopBtn);
-      this.show(this.speakContentBtn);
-    }
-    this.show(this.interfaceEl);
-  }
-
   addListeners(){
-    this.speakContentBtn.addEventListener('click', this.speakContentHandler.bind(this));
-    this.stopBtn.addEventListener('click', this.stopSpeakingHandler.bind(this));
-    this.jumpToCommentBtn.addEventListener('click', this.jumpToCommentHandler.bind(this));
     if(typeof chrome !== 'undefined'){
       chrome.runtime.onMessage.addListener(
         function(request, sender, sendResponse) {
           switch(request.message){
-            case 'commentCount':
-              this.count = request.data.count;
-              this.commentCountHandler(this.count);
-              break;
-            case 'currentComment':
-              this.currentComment = request.data.currentComment;
-              this.currentCommentHandler(this.currentComment);
-              break;
             case 'parseFail':
               this.parseFailHandler();
               break;
             case 'canParse':
-              this.initInterface(request.data.speaking);
+              this.closeWindow();
           }
         }.bind(this)
       );
